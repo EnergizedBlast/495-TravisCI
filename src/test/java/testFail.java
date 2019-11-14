7df7a06210f7dc5a6df14b1484cdd5a88988af8e
BuildTools <unconfigured@null.spigotmc.org>
2019-11-14 15:05:24 -0500

    Add failing test

---
+++ src/test/java/testFail.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class testFail {
+
+    @Test
+    void factorial(){
+        Factorial testClass = new Factorial();
+        int testValue = testClass.factorial(3);
+        assertEquals(testValue, 5);
+    }
+
+}
